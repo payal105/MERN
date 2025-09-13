9c3203f5aeca2cbb2290c4afa44b6238b7924482
Payal Adhikary <payaladhikary2000@gmail.com>
2025-09-13 12:11:49 +0530

    Frontend Completed

--- Greencart/client/src/App.jsx
+++ Greencart/client/src/App.jsx
@@ -12,13 +12,19 @@ import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
 import AddAddres from './pages/AddAddres'
 import MyOrders from './pages/MyOrders'
+import SellerLogin from './components/Seller/SellerLogin'
+import SellerLayout from './pages/Seller/SellerLayout'
+import AddProduct from './pages/Seller/AddProduct'
+import ProductList from './pages/Seller/ProductList'
+import Orders from './pages/Seller/Orders'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes('seller');
-  const {showUserLogin} = useAppContext();
+  const {showUserLogin,isSeller} = useAppContext();
+  
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
       {isSellerPath? null : <Navbar />}
       {showUserLogin? <Login /> : null}
       <Toaster />
@@ -31,6 +37,11 @@ const App = () => {
           <Route path='/cart' element={<Cart />}/>
           <Route path='/add-address' element={<AddAddres />}/>
           <Route path='/my-orders' element={<MyOrders />}/>
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+            <Route index element={isSeller ? <AddProduct/> : null} />
+            <Route path='product-list' element={<ProductList/>} />
+            <Route path='orders' element={<Orders/>} />
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
